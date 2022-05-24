90f7d8343af03488df2ff8dc79204ee2e3bb70ca
devmohnihshsharma <devmohnishsharma@gmail.com>
2022-05-24 08:21:08 +0530

    Graphs study and practice

--- Trees/Implementation/binarySearchTree.js
+++ Trees/Implementation/binarySearchTree.js
@@ -1,61 +1,3 @@
-// class Node {
-//     constructor(value){
-//       this.left = null;
-//       this.right = null;
-//       this.value = value;
-//     }
-//   }
-  
-//   class BinarySearchTree {
-//     constructor(){
-//       this.root = null;
-//     }
-    
-//     insert(value){
-//         const newNode = new Node(value)
-//         // Check first if we have root node or not 
-//         if(this.root === null){
-//            return this.root = newNode
-//         }
-
-//         else{
-//             let currentNode = this.root
-//             while(true){
-//                 if(value < currentNode.value){
-//                     if(!currentNode.left ){
-//                         currentNode.left = newNode
-//                         return this
-//                     }
-//                    currentNode = currentNode.left
-//                 }else{
-//                     if(!currentNode.right){
-//                         currentNode.right = newNode;
-//                         return this
-//                     }
-//                     currentNode = currentNode.right
-                    
-//                 }
-//             }
-//         }
-//     }
-//     // remove
-//   }
-  
-//   const tree = new BinarySearchTree();
-//   tree.insert(9)
-//   tree.insert(4)
-//   tree.insert(6)
-//   tree.insert(20)
-//   tree.insert(170)
-//   tree.insert(15)
-// console.log(tree.insert(1));  
-//   //     9
-//   //  4     20
-//   //1  6  15  170
-  
-
-
-
 class Node {
   constructor(value){
     this.left = null;
@@ -104,6 +46,55 @@ class BinarySearchTree {
   }
     return this 
   }
+// My lookup solution....
+  // lookup(value){
+  //   if(this.root === null){
+  //     return false
+  //   }else{
+  //     let currentNode  = this.root;
+
+  //     while(true){
+  //       if(currentNode.value < value){
+  //         if(currentNode.value === value){
+  //           console.log(currentNode , "yes we find the value... left");
+  //             return true
+  //         }
+  //         if(currentNode.left){
+  //           currentNode = currentNode.left
+  //         }else{
+  //           return false
+  //         }
+  //       }else{
+  //         if(currentNode.value === value){
+  //           console.log(currentNode , "yes we find the value... right");
+  //           return true
+  //         }
+  //         if(currentNode.right){
+  //           currentNode = currentNode.right
+  //         }else{
+  //           return false
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
+  lookup(value){
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while(currentNode){
+      if(value < currentNode.value){
+        currentNode = currentNode.left;
+      } else if(value > currentNode.value){
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return null
+  }
 
 }
 
@@ -111,4 +102,5 @@ const tree = new BinarySearchTree();
   tree.insert(6)
   tree.insert(20)
   tree.insert(170)
-console.log(tree.insert(15));
+  tree.insert(15)
+console.log(tree.lookup(20));
